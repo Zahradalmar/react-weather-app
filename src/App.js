0bb9383955a2bb1be9fda9c39c19245204be6d7e
Zahradalmar <zahradalmar1@gmail.com>
2022-08-26 17:19:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by 👉🏼 Zahra Mohamed and is {" "}
-        <a href="https://github.com/Zahradalmar/react-weather-app" target="_blank">
+        <a href="https://github.com/Zahradalmar/react-weather-app" target="_blank" rel="noreferrer">
            open-sourced on GitHub
         </a>
       </footer>
